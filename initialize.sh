e378081bbfb5f3a9c7c277cef0e96beb041c872b
John Wadleigh <wadleigh.john@gmail.com>
2022-11-12 12:19:03 -0800

    Fixed various issues with script

--- initialize.sh
+++ initialize.sh
@@ -1,75 +1,55 @@
 #!/bin/bash
 
+task () {
+    echo -e "\n\n"
+    echo "TASK: [$1] ******************************"
+}
+
+echo 
 echo "OSX Provisioner"
 echo "Initialize system with base required software"
 
-
-echo "TASK: [Verify Xcode command line tools installation...] ******************"
-OSXP_OUTPUT="$(xcode-select -p)"
-OSXP_PATH="/Library/Developer/CommandLineTools"
-if [[ -d $OSXP_OUTPUT && $OSXP_OUTPUT =~ $OSXP_PATH* ]]
+task "Verify Xcode command line tools installation"
+AMP_XCODE_OUTPUT=`xcode-select -p`
+AMP_XCODE_PATH="/Library/Developer/CommandLineTools"
+if [[ $AMP_XCODE_OUTPUT == $AMP_XCODE_PATH ]]
 then
-    echo -e "Found:" $OSXP_OUTPUT "\n\n"
+    echo -e "Found Xcode: " $AMP_XCODE_OUTPUT
+    echo "Found gcc: " `which gcc`
+    echo "Found git: " `which git`
 else
-    echo $OSXP_OUTPUT
-    echo -e "WARNING: Xcode Command Line Tools are required. Attempting installation...\n"
+    echo -e "WARNING: Xcode Command Line Tools not found or needs to be re-installed. Attempting installation...\n"
     xcode-select --install
-    exit 1
 fi
 
 
-
-echo "TASK: [Verify Xcode command line tools installation...] ******************"
-echo "Found gcc: " `which gcc`
-echo "Found git: " `which git`
-
 # list all available xcode command line tools
 #ls /Library/Developer/CommandLineTools/usr/bin
-echo ""
-#echo "TASK: [Verify pip installation...] ******************"
-# python
-#python --version
-# pip
-#sudo easy_install pip
 
-
-echo "TASK: [Verify Homebrew installation...] ******************"
-OSXP_OUTPUT="$(which brew)"
-OSXP_PATH="/usr/local/bin/brew"
-if [[ "$OSXP_OUTPUT" == "$OSXP_PATH" ]]
+task "Verify Homebrew installation"
+AMP_BREW_OUTPUT="$(which brew)"
+AMP_BREW_PATH="/usr/local/bin/brew"
+if [[ "$AMP_BREW_OUTPUT" == "$AMP_BREW_PATH" ]]
 then
     echo -e "Found homebrew: " `which brew`
 else
     echo -e "Brew was not found ... attempting installation\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
-echo ""
 
-#echo "TASK: [Verify Ansible installation...] ******************"
-# ansible
-#pip install ansible
-#pip install --upgrade ansible
-# verify 
-#brew install ansible
-
-
-echo "TASK: [Perform brew cleanup tasks ...] ******************"
+task "Perform brew cleanup tasks"
 brew cleanup
 brew doctor
 
-echo ""
-echo -e "All checks completed. Ready to start provisioning your system.\n\n"
-echo ""
+task "Prepare python environment"
+#TODO: Check if pyenv is installed and ansible is available (but careful about not upgrading)
+brew install --quiet pyenv
+brew install --quiet pyenv-virtualenv
 
-
-echo "TASK: [Begin base provisioning ...] ******************"
-brew install pyenv
-brew install pyenv-virtualenv
-
-OSXP_PYENV_VERSION="$(pyenv version-name)"
-if [[ "$OSXP_PYENV_VERSION" == "ansible" ]]
+AMP_PYENV_VERSION=`pyenv version-name`
+if [[ "$AMP_PYENV_VERSION" == "ansible" ]]
 then
-    echo -e "Found ansible virtual environment: " $OSXP_PYENV_VERSION
+    echo -e "Found ansible virtual environment: " $AMP_PYENV_VERSION
 else
     echo -e "`pyenv` was not initialized ... attempting that now\n"
     # Install the latest version of python that is available
@@ -81,16 +61,14 @@ else
     pyenv global ansible
     pip install ansible
 fi
-echo ""
 
-# Bash Profile
+task "Prepare bash environment"
 cp data/.bash_profile ~/.bash_profile
 chsh -s /bin/bash
 source ~/.bash_profile
 
-# SSH Configuration
+task "Prepare ssh configuration"
 cp data/ssh.config ~/.ssh/config
 
-echo ""
-echo "Success"
+task "Completed"
 exit 0
