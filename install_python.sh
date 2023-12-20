af0c44eb1fdbd3040b90a570961c7d409f8ee608
John Wadleigh <wadleigh.john@gmail.com>
2023-12-20 09:36:39 -0800

    Improved python virtual env configuration

--- install_python.sh
+++ install_python.sh
@@ -16,10 +16,9 @@ then
 else
     echo -e "Install the latest version of python that is available using pyenv"
     pyenv install --skip-existing $(pyenv install --list | grep --extended-regexp "^\s*[0-9][0-9.]*[0-9]\s*$" | tail -1)
-    # Set the python version as the global default
-    pyenv global $(pyenv install --list | grep --extended-regexp "^\s*[0-9][0-9.]*[0-9]\s*$" | tail -1)
-    # Create virtual environment for our ansible tools
-    pyenv virtualenv dev
+    # Create virtual environment
+    pyenv virtualenv $(pyenv install --list | grep --extended-regexp "^\s*[0-9][0-9.]*[0-9]\s*$" | tail -1) dev
+    # Set the global default python environment
     pyenv global dev
     pip install --upgrade pip
     pip install ansible ansible-lint ansible-navigator ansible-builder
